3b2c1d2777c43d3f3ff189f1b49d9a892688e518
Apple <apple@Apples-MacBook-Pro-4.local>
2019-06-09 16:48:08 +0530

    Updates

---
+++ precache-manifest.2f0d10b58622e64ee1ec2400f427dd94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72fab5fd7a3165319030459e3921ddc9",
+    "url": "/frappe-app/index.html"
+  },
+  {
+    "revision": "4e53f82aca1fbfe77b66",
+    "url": "/frappe-app/static/css/main.a84d14e1.chunk.css"
+  },
+  {
+    "revision": "3ce6b71c38f69fcdbafa",
+    "url": "/frappe-app/static/js/2.8527b308.chunk.js"
+  },
+  {
+    "revision": "4e53f82aca1fbfe77b66",
+    "url": "/frappe-app/static/js/main.af32336c.chunk.js"
+  },
+  {
+    "revision": "4ff520f3c1672d1f7a7d",
+    "url": "/frappe-app/static/js/runtime~main.4648de2c.js"
+  }
+]);
